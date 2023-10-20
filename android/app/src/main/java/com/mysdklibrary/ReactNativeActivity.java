8ba4c6f994482db896b66ec1c83b1a8514a4a142
Saikrishna Kotagiri <saikrishnakotagiri@apples-MacBook-Pro.local>
2023-10-20 21:39:35 +0530

    first commit

---
+++ android/app/src/main/java/com/mysdklibrary/ReactNativeActivity.java
@@ -0,0 +1,10 @@
+package com.mysdklibrary;
+
+import com.facebook.react.ReactActivity;
+
+public class ReactNativeActivity extends ReactActivity {
+    @Override
+    protected String getMainComponentName() {
+        return "mysdklibrary";
+    }
+}
